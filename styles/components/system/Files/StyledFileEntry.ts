337d270e9170ffdce807f6aef2b1db364aa9d165
Dustin Brett <dustinbrett@gmail.com>
2021-03-07 21:33:10 -0800

    Styling for 1 line desktop icons

--- styles/components/system/Files/StyledFileEntry.ts
+++ styles/components/system/Files/StyledFileEntry.ts
@@ -1,5 +1,42 @@
 import styled from 'styled-components';
 
-const StyledFileEntry = styled.li``;
+const StyledFileEntry = styled.li`
+  display: flex;
+  padding: 2px;
+  place-content: center;
+
+  &:hover {
+    background-color: hsla(0, 0%, 50%, 25%);
+    border: 2px solid hsla(0, 0%, 50%, 25%);
+    padding: 0;
+    position: relative;
+
+    &::before {
+      border: 1px solid hsla(0, 0%, 70%, 55%);
+      bottom: -1px;
+      content: '';
+      left: -1px;
+      position: absolute;
+      right: -1px;
+      top: -1px;
+    }
+  }
+
+  figcaption {
+    color: #fff;
+    font-size: 11.5px;
+    margin-top: -2px;
+    text-shadow: 0 0 1px rgba(0, 0, 0, 75%), 0 0 2px rgba(0, 0, 0, 50%),
+      0 0 3px rgba(0, 0, 0, 25%), 0 1px 1px rgba(0, 0, 0, 75%),
+      0 1px 2px rgba(0, 0, 0, 50%), 0 1px 3px rgba(0, 0, 0, 25%),
+      0 2px 1px rgba(0, 0, 0, 75%), 0 2px 2px rgba(0, 0, 0, 50%),
+      0 2px 3px rgba(0, 0, 0, 25%);
+  }
+
+  img {
+    height: 48px;
+    width: 48px;
+  }
+`;
 
 export default StyledFileEntry;
--- styles/components/system/Files/StyledFileManager.ts
+++ styles/components/system/Files/StyledFileManager.ts
@@ -1,5 +1,15 @@
 import styled from 'styled-components';
 
-const StyledFileManager = styled.ol``;
+const StyledFileManager = styled.ol`
+  column-gap: 1px;
+  display: grid;
+  grid-auto-flow: column;
+  grid-template-columns: repeat(auto-fill, 74px);
+  grid-template-rows: repeat(auto-fill, 70px);
+  height: calc(100% - 30px);
+  padding: 5px 0;
+  row-gap: 28px;
+  width: 100%;
+`;
 
 export default StyledFileManager;
