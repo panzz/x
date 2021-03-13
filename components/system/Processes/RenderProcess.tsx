b4b9a1fe36d8a411398c5852f32f26755e395784
Dustin Brett <dustinbrett@gmail.com>
2021-03-13 08:36:38 -0800

    No more withWindow or default props linting

--- components/system/Processes/RenderProcess.tsx
+++ components/system/Processes/RenderProcess.tsx
@@ -2,21 +2,21 @@ import dynamic from 'next/dynamic';
 
 const Window = dynamic(() => import('components/system/Window'));
 
-const withWindow = (Component: React.ComponentType) => (
-  <Window>
-    <Component />
-  </Window>
-);
-
 type RenderProcessProps = {
   Component: React.ComponentType;
-  hasWindow: boolean;
+  hasWindow?: boolean;
 };
 
 const RenderProcess = ({
   Component,
-  hasWindow
+  hasWindow = false
 }: RenderProcessProps): JSX.Element =>
-  hasWindow ? withWindow(Component) : <Component />;
+  hasWindow ? (
+    <Window>
+      <Component />
+    </Window>
+  ) : (
+    <Component />
+  );
 
 export default RenderProcess;
