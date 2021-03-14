4d13f250e52531600b51d1a7eb6d6c49ec635180
Dustin Brett <dustinbrett@gmail.com>
2021-03-13 20:37:29 -0800

    Added resizing of windows

--- components/system/Window/index.tsx
+++ components/system/Window/index.tsx
@@ -1,20 +1,31 @@
 import Titlebar from 'components/system/Window/Titlebar';
 import { useProcesses } from 'contexts/process';
+import useResizeable from 'hooks/useResizeable';
+import { Rnd } from 'react-rnd';
 import StyledWindow from 'styles/components/system/Window/StyledWindow';
 import type { ProcessComponentProps } from 'types/contexts/process';
+import rndDefaults from 'utils/rndDefaults';
 
 const Window: React.FC<ProcessComponentProps> = ({ children, id }) => {
   const {
     processes: {
-      [id]: { minimized }
+      [id]: { maximized, minimized }
     }
   } = useProcesses();
+  const { height, width, updateSize } = useResizeable(maximized);
 
   return (
-    <StyledWindow minimized={minimized}>
-      <Titlebar id={id} />
-      {children}
-    </StyledWindow>
+    <Rnd
+      enableResizing={!maximized}
+      onResize={updateSize}
+      size={{ height, width }}
+      {...rndDefaults}
+    >
+      <StyledWindow minimized={minimized}>
+        <Titlebar id={id} />
+        {children}
+      </StyledWindow>
+    </Rnd>
   );
 };
 
